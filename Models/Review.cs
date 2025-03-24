478296d29c98b85aade99f5d704791956c3c0b80
NiklasRoos91 <niklas_roos@live.se>
2025-03-24 11:01:47 +0100

    Made models and controllers

---
+++ Models/Review.cs
@@ -0,0 +1,9 @@
+﻿namespace GameJournal.Models
+{
+    public class Review
+    {
+        public string GameId { get; set; }
+        public int Grade { get; set; }
+        public string Comment { get; set; }
+    }
+}
