932e1e6f8e43b08a9f73b62c803103cd35fed5e5
NiklasRoos91 <niklas_roos@live.se>
2025-04-01 14:17:44 +0200

    Added FluentValidation classes

---
+++ Validators/GameValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using GameJournal.DTOs;
+
+namespace GameJournal.Validators
+{
+    public class GameValidator : AbstractValidator<GameDto>
+    {
+        public GameValidator()
+        {
+            string[] allowedGenres = new string[] { "Action", "Adventure", "RPG", "Strategy", "Puzzle", "Shooter" };
+            string[] alloweStatuses = new string[] { "Not Started", "In Progress", "Completed" };
+
+
+            RuleFor(gameDto => gameDto.Title)
+                .NotEmpty().WithMessage("Title cannot be empty")
+                .MaximumLength(200).WithMessage("Title cannot exceed 200 charachtgers");
+
+            RuleFor(gameDto => gameDto.Genre)
+                .NotEmpty().WithMessage("Genre is required.")
+                .Must(genre => allowedGenres.Contains(genre)).WithMessage("Invalid genre. Allowed genres are: Action, Adventure, RPG, Strategy, Puzzle, Shooter.");
+
+            RuleFor(gameDto => gameDto.Status)
+                .NotEmpty().WithMessage("Status Cannot be empty")
+                .Must(status => alloweStatuses.Contains(status)).WithMessage("Invalid status. Allowed statuses are: Not Started, In Progress, Completed");
+        }
+    }
+}
---
+++ Validators/ReviewValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using GameJournal.DTOs;
+
+namespace GameJournal.Validators
+{
+    public class ReviewValidator : AbstractValidator<ReviewDto>
+    {
+        public ReviewValidator()
+        {
+            RuleFor(ReviewDto => ReviewDto.Comment)
+                .NotEmpty().WithMessage("Comment Cannot be empty")
+                .MaximumLength(500).WithMessage("Comment cannot exceed 500 charachters");
+
+            RuleFor(reviewDto => reviewDto.Grade)
+                .InclusiveBetween(1, 5).WithMessage("Grade must be between 1 and 5.");
+        }
+    }
+}
